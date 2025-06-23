dcd2e5379e14faa6c3e2d33b0ee4b9081b0dbaa8
Abjithbk <bkabjith2@gmail.com>
2025-06-23 23:53:47 +0530

    show transaction is complete with few logic error

--- src/components/UserDashboard.jsx
+++ src/components/UserDashboard.jsx
@@ -9,12 +9,13 @@ const UserDashboard = () => {
 
   const [openMenu, setOpenMenu] = useState(false);
   const [activeSection,setActiveSection] = useState("dashboard")
-  // const [transaction,setTransaction] = useState([]);
+  const [transactions,setTransactions] = useState([]);
   const [amount,setAmount] = useState("");
   const [type,setType] = useState("deposit")
   const [accountNumber,setAccountNumber] = useState('')
   const [receiverAccountNumber, setReceiverAccountNumber] = useState('');
   const [description, setDescription] = useState('');
+  const [selectDetails,setSelectDetails] = useState('')
   const [error,setError] = useState('')
   const [userProfile,setUserProfile] = useState(null)
   const navigate = useNavigate();
@@ -43,6 +44,16 @@ const UserDashboard = () => {
       })
       console.log(profileDetails.data);
       setUserProfile(profileDetails.data);
+      const sender = profileDetails.data.account.sender;
+      const receiver = profileDetails.data.account.receiver;
+
+      const combinedDetails = [...sender,...receiver].map((txn) => (
+        {
+          ...txn, 
+          direction : sender.includes(txn) ? "send" : "received"
+        }
+      ))
+      setTransactions(combinedDetails)
      }
      catch(error) {
       console.log(error);
@@ -76,7 +87,49 @@ const UserDashboard = () => {
     setError(error.response.data.error)
     
     }
-  }
+  }  
+    const handleTransactionClick =async (txn) => {
+      // const [senderResponse,receiverResponse] = await Promise.all([
+      //    axios.get(`https://village-banking-app.onrender.com/api/profile/${txn.sender}/`, {
+      //     headers : {
+      //       Authorization : `Bearer ${authToken?.access}`,
+      //     }
+      //    }),
+      //    axios.get(`https://village-banking-app.onrender.com/api/profile/${txn.receiver}/`, {
+      //     headers : {
+      //       Authorization : `Bearer ${authToken?.access}`,
+      //     }
+      //    })
+      // ])
+      const requests = [];
+
+      if(txn.sender) {
+        requests.push(
+         axios.get(`https://village-banking-app.onrender.com/api/profile/${txn.sender}/`, {
+          headers : {
+            Authorization : `Bearer ${authToken?.access}`
+          }
+        })
+        )
+      }
+
+      if(txn.receiver) {
+        requests.push(
+           axios.get(`https://village-banking-app.onrender.com/api/profile/${txn.receiver}/`, {
+          headers : {
+            Authorization : `Bearer ${authToken?.access}`
+          }
+        })
+        )
+      }
+      const [senderResponse,receiverResponse] = await Promise.all(requests)
+      setSelectDetails({
+        ...txn,
+        senderDetails : senderResponse.data,
+        receiverDetails : receiverResponse.data,
+      })
+
+    }
 
   return (
     <div className="flex flex-col md:flex-row min-h-screen">
@@ -144,35 +197,153 @@ const UserDashboard = () => {
 
       
         {activeSection === "transactions" && (
-          <>
-            <div className="overflow-x-auto">
-              <table className="min-w-full border text-sm">
-                <thead className="bg-gray-200 text-left">
-                  <tr>
-                    <th className="p-3">ID</th>
-                    <th className="p-3">Type</th>
-                    <th className="p-3">Amount</th>
-                    <th className="p-3">Date</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr className="border-t">
-                    <td className="p-3">1</td>
-                    <td className="p-3">Deposit</td>
-                    <td className="p-3">₹ 1,000</td>
-                    <td className="p-3">2025-06-17</td>
-                  </tr>
-                  <tr className="border-t">
-                    <td className="p-3">2</td>
-                    <td className="p-3">Withdraw</td>
-                    <td className="p-3">₹ 500</td>
-                    <td className="p-3">2025-06-18</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-          </>
-        )}
+  <>
+    <h3 className="text-xl font-bold mb-4">Sent Transactions</h3>
+    <div className="overflow-x-auto mb-6">
+      <table className="min-w-full text-sm border rounded">
+        <thead className="bg-gray-200">
+          <tr>
+            <th className="p-2">Type</th>
+            <th className="p-2">Amount</th>
+            <th className="p-2">Status</th>
+            <th className="p-2">Date</th>
+            <th className="p-2">Details</th>
+          </tr>
+        </thead>
+        <tbody>
+          {transactions.map((txn, index) => (
+            <tr onClick={()=> {
+              handleTransactionClick(txn);
+            }} key={index} className="border-t">
+              <td className="p-2">{txn.transaction_type}</td>
+              <td className="p-2">₹ {txn.amount}</td>
+              <td className="p-2">{txn.status}</td>
+              <td className="p-2">{txn.timestamp}</td>
+              <td className="p-2">View</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>  
+       {
+        selectDetails&& (
+           <div className="mt-6 bg-white border p-4 rounded shadow">
+        <h4 className="text-lg font-semibold mb-2">Full Details</h4>
+        {
+          selectDetails.senderDetails && (
+ <>
+              <div className="bg-white shadow p-6 rounded-lg max-w-3xl mx-auto w-full">
+                  <h2 className="text-2xl font-bold mb-6 text-center">Sender</h2>
+
+                  <div className="flex flex-col md:flex-row items-center md:items-start gap-6">
+                  
+                    <img
+                      src={selectDetails.senderDetails.user.profile_pic}
+                      alt="Profile"
+                      className="w-40 h-40 rounded-full object-cover border-4 border-blue-200 shadow-md"
+                    />
+
+                    
+                    <div className="flex-1 w-full">
+                      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                        <div>
+                          <p className="text-gray-600 text-sm">First Name</p>
+                          <p className="text-lg font-medium">{selectDetails.senderDetails.user.first_name}</p>
+                        </div>
+                        <div>
+                          <p className="text-gray-600 text-sm">Last Name</p>
+                          <p className="text-lg font-medium">{selectDetails.senderDetails.user.last_name}</p>
+                        </div>
+                        <div>
+                          <p className="text-gray-600 text-sm">Age</p>
+                          <p className="text-lg font-medium">{selectDetails.senderDetails.user.age}</p>
+                        </div>
+                        <div>
+                          <p className="text-gray-600 text-sm">Phone Number</p>
+                          <p className="text-lg font-medium">{selectDetails.senderDetails.user.phonenumber}</p>
+                        </div>
+                        <div>
+                          <p className="text-gray-600 text-sm">Account Number</p>
+                          <p className="text-lg font-medium">{selectDetails.senderDetails.account_number}</p>
+                        </div>
+                        <div>
+                          <p className="text-gray-600 text-sm">Account Balance</p>
+                          <p className="text-lg font-medium">₹ {selectDetails.senderDetails.balance} </p>
+                        </div>
+                        <div className="md:col-span-2">
+                          <p className="text-gray-600 text-sm">Created At</p>
+                          <p className="text-lg font-medium">
+                            {selectDetails.senderDetails.created_at}
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+        </>
+          )
+        }
+       {
+        selectDetails.receiverDetails && (
+<>
+         <div className="bg-white shadow p-6 rounded-lg max-w-3xl mx-auto w-full">
+                  <h2 className="text-2xl font-bold mb-6 text-center">Receiver</h2>
+
+                  <div className="flex flex-col md:flex-row items-center md:items-start gap-6">
+                  
+                    <img
+                      src={selectDetails.receiverDetails.user.profile_pic}
+                      alt="Profile"
+                      className="w-40 h-40 rounded-full object-cover border-4 border-blue-200 shadow-md"
+                    />
+
+                    
+                    <div className="flex-1 w-full">
+                      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                        <div>
+                          <p className="text-gray-600 text-sm">First Name</p>
+                          <p className="text-lg font-medium">{selectDetails.receiverDetails.user.first_name}</p>
+                        </div>
+                        <div>
+                          <p className="text-gray-600 text-sm">Last Name</p>
+                          <p className="text-lg font-medium">{selectDetails.receiverDetails.user.last_name}</p>
+                        </div>
+                        <div>
+                          <p className="text-gray-600 text-sm">Age</p>
+                          <p className="text-lg font-medium">{selectDetails.receiverDetails.user.age}</p>
+                        </div>
+                        <div>
+                          <p className="text-gray-600 text-sm">Phone Number</p>
+                          <p className="text-lg font-medium">{selectDetails.receiverDetails.user.phonenumber}</p>
+                        </div>
+                        <div>
+                          <p className="text-gray-600 text-sm">Account Number</p>
+                          <p className="text-lg font-medium">{selectDetails.receiverDetails.account_number}</p>
+                        </div>
+                        <div>
+                          <p className="text-gray-600 text-sm">Account Balance</p>
+                          <p className="text-lg font-medium">₹ {selectDetails.receiverDetails.balance} </p>
+                        </div>
+                        <div className="md:col-span-2">
+                          <p className="text-gray-600 text-sm">Created At</p>
+                          <p className="text-lg font-medium">
+                            {selectDetails.receiverDetails.created_at}
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+        </>
+        )
+       }
+        
+      </div>
+        )
+       }
+  </>
+)}
+
           {
             activeSection === "perform" && (
               <form onSubmit={handleTransaction} className="bg-blue-50 border border-blue-200 p-4 rounded-md max-w-xl mb-6">
